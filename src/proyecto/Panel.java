2bbe86a0b32de52d4ea5d6f713c6fc14051d5b77
1Shiro <nose.x2xd@gmail.com>
2022-11-21 19:19:27 -0300

    creacion de panel

---
+++ src/proyecto/Panel.java
@@ -0,0 +1,31 @@
+
+package proyecto;
+
+import static java.awt.Color.GREEN;
+import javax.swing.JPanel;
+import java.awt.*;
+
+
+public class Panel extends JPanel {
+    int[] puntoX;
+    int[] puntoY;
+    
+    public Panel(){
+        super();
+        this.setBackground(GREEN);
+          
+        this.puntoX = new int[]{100,200,200,300,300,200,200,300,300,200,200,100,100,0,0,100,100,0,0,100};
+        this.puntoY = new int[]{100,100,200,200,300,300,400,400,500,500,560,560,500,500,400,400,300,300,200,200};
+        
+        
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.drawPolygon(puntoX,puntoY,20);
+    }
+    
+            
+}
--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -14,7 +14,7 @@ public class Proyecto {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ventana ventana = new Ventana();
     }
     
 }
