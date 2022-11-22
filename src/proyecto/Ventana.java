218e8472b928e43d5a66ad3cc530e1c29847fdaf
yeittcl <raulzapatavillalobos@gmail.com>
2022-11-22 18:34:21 -0300

    mejorando clase ventana

--- src/proyecto/Ventana.java
+++ src/proyecto/Ventana.java
@@ -1,24 +1,18 @@
-
 package proyecto;
 
 import java.awt.*;
 import javax.swing.*;
 
 public class Ventana extends JFrame{
-    Panel juan;
-    public Ventana(){
+    public Ventana(Panel panel){
         super();
-        juan = new Panel();
-        
         this.setLayout(new BorderLayout());
         this.setTitle("juego");
         this.setSize(1000,1000);
-        this.add(juan);
+        this.add(panel);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
-        
-        
         this.setVisible(true);
         
     } 
