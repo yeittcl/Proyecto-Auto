3295c42706831689e00120b6ac810ea8461c2e5e
yeittcl <raulzapatavillalobos@gmail.com>
2022-11-22 18:26:43 -0300

    arreglo del error2 en git

--- src/proyecto/Proyecto.java
+++ src/proyecto/Proyecto.java
@@ -3,11 +3,6 @@ package proyecto;
 public class Proyecto {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        
-=======
-        Ventana v1 = new Ventana();
->>>>>>> 08b2dc985edbc5abc052f91ed24714bdb6077570
     }
     
 }
