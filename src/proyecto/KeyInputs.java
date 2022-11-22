ae078076f3480d761ab0259646b94397912ec322
yeittcl <raulzapatavillalobos@gmail.com>
2022-11-22 18:39:14 -0300

    creando clase KeyInputs para los eventos de teclado

---
+++ src/proyecto/KeyInputs.java
@@ -0,0 +1,24 @@
+package proyecto;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyInputs implements KeyListener{
+    private Panel panel;
+    public KeyInputs(Panel panel){
+        this.panel = panel;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+    
+}
