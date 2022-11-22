8d5d3edd27d3b07a5c3aad6c94c000ca0109bc4b
yeittcl <raulzapatavillalobos@gmail.com>
2022-11-22 18:37:09 -0300

    pintando una figura mas facil de probar

--- src/proyecto/Panel.java
+++ src/proyecto/Panel.java
@@ -1,29 +1,21 @@
-
 package proyecto;
 
 import java.awt.*;
 import javax.swing.JPanel;
 import java.awt.*;
 
-
 public class Panel extends JPanel {
-    int[] puntoX;
-    int[] puntoY;
     
     public Panel(){
         super();
         this.setBackground(Color.GREEN);
-          
-        this.puntoX = new int[]{100,200,200,300,300,200,200,300,300,200,200,100,100,0,0,100,100,0,0,100};
-        this.puntoY = new int[]{100,100,200,200,300,300,400,400,500,500,560,560,500,500,400,400,300,300,200,200};
-        
     }
     
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.setColor(Color.BLACK);
-        g.drawPolygon(puntoX,puntoY,20);
+        g.fillRect(300, 300, 100, 100);
     }
     
             
