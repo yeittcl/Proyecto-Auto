9a61beac39b38df978c65a71eb5d1107e219056f
yeittcl <raulzapatavillalobos@gmail.com>
2022-11-22 18:47:19 -0300

    implementando thread en game para el game loop

--- src/proyecto/Game.java
+++ src/proyecto/Game.java
@@ -1,12 +1,20 @@
 package proyecto;
 
-public class Game {
+public class Game implements Runnable{
     private Ventana ventana;
     private Panel panel;
+    private Thread gameThread;
     
     public Game(){
         panel = new Panel();
         ventana = new Ventana(panel);
         panel.requestFocus();
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        
     }
 }
