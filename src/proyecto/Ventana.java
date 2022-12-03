d5cdc01f36f1d9239a10403ec1c20c58654b5dd4
yeittcl <raulzapatavillalobos@gmail.com>
2022-12-03 20:45:39 -0300

    reescalando la ventana

--- src/proyecto/Ventana.java
+++ src/proyecto/Ventana.java
@@ -8,7 +8,7 @@ public class Ventana extends JFrame{
         super();
         this.setLayout(new BorderLayout());
         this.setTitle("juego");
-        this.setSize(1000,1000);
+        this.setSize(1280,720);
         this.add(panel);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
