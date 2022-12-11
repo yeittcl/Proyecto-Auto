75b719d26473b68956f6d9063a306d295db63e40
1Shiro <nose.x2xd@gmail.com>
2022-12-10 23:26:09 -0300

    implementacion de paneles de pista

--- src/Config/PanelConfig.java
+++ src/Config/PanelConfig.java
@@ -1,17 +1,33 @@
 package Config;
-
 import java.awt.*;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 
+public class PanelConfig extends JPanel{
+    private PanelPistaExterior panelSliderExterior;
+    private PanelPistaInterior panelSliderInterior;
+    private JLabel labelExterior;
+    private JLabel labelInterior;
 
-public class PanelConfig extends JPanel {
-    public PanelConfig(){
+    public PanelConfig(PistaHolder pistaHolderExterior, PistaHolder pistaHolderInterior){
         super();
         this.setLayout(null);
-        this.setBackground(Color.WHITE);
-        
+        this.setBackground(Color.white);
+
+        labelExterior = new JLabel("Radio pista exterior");
+        labelExterior.setHorizontalAlignment(SwingConstants.CENTER);
+        labelExterior.setBounds(250, 100, 190, 100);
+
+        panelSliderExterior = new PanelPistaExterior(pistaHolderExterior);
+        this.add(labelExterior);
+        this.add(panelSliderExterior);
+
+        labelInterior = new JLabel("Radio pista interior");
+        labelInterior.setHorizontalAlignment(SwingConstants.CENTER);
+        labelInterior.setBounds(800, 100, 190, 100);
+
+        panelSliderInterior = new PanelPistaInterior(pistaHolderInterior);
+        this.add(labelInterior);
+        this.add(panelSliderInterior);
     }
 }
-
-
